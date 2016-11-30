4296132f70569462af8e8da0e5efe5e12e749cd7
Davood Mazinanian <dmazinanian@gmail.com>
2016-11-30 13:39:33 -0500

    Commit 1
    Addition of a Consumer with block and no parameter type

--- src/ca/concordia/lambda/test/TestClass.java
+++ src/ca/concordia/lambda/test/TestClass.java
@@ -1,7 +1,13 @@
 package ca.concordia.lambda.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestClass {
 	public void testMethodA() {
-
+		List<String> list = new ArrayList<>();
+		list.forEach(item -> {
+			System.out.println(item);
+		});
 	}
 }
