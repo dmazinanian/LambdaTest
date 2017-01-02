b34b16a3a9a8d09dcbec914ea4c806a7f85df480
Davood Mazinanian <dmazinanian@gmail.com>
2017-01-02 14:19:56 -0500

    Lambda in existing method

--- src/ca/concordia/lambda/test/TestClass3.java
+++ src/ca/concordia/lambda/test/TestClass3.java
@@ -9,5 +9,7 @@ public class TestClass3 {
 	
 	public void testMethod2() {
 		Arrays.asList("Test1", "Test2").replaceAll(s1 -> s1.trim());
+		Arrays.asList("Test1", "Test2").replaceAll(s1 -> s1.trim());
 	}
+	
 }
