5236fa2d3e8c08b2dfdfcb6b9daae03e659b180b
Davood Mazinanian <dmazinanian@gmail.com>
2017-01-02 14:19:27 -0500

    Lambda in new method

--- src/ca/concordia/lambda/test/TestClass3.java
+++ src/ca/concordia/lambda/test/TestClass3.java
@@ -6,4 +6,8 @@ public class TestClass3 {
 	public void testMethod() {
 		Arrays.asList("Test1", "Test2").replaceAll(s1 -> s1.trim());
 	}
+	
+	public void testMethod2() {
+		Arrays.asList("Test1", "Test2").replaceAll(s1 -> s1.trim());
+	}
 }
